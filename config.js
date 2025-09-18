9e1213c7b7ffdc739e56ab5c1cb7438fd34c7a53
ali-cmd-dot <ali@cautio.in>
2025-09-18 17:12:29 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window.CAUTIO_CONFIG = {
+    supabase: {
+        url: 'https://jcmjazindwonrplvjwxl.supabase.co',
+        key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjbWphemluZHdvbnJwbHZqd3hsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczMDEyNjMsImV4cCI6MjA3Mjg3NzI2M30.1B6sKnzrzdNFhvQUXVnRzzQnItFMaIFL0Y9WK_Gie9g'
+    },
+    app: {
+        name: 'Cautio',
+        version: '1.0.0'
+    }
+};
